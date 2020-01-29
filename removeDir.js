7403b0e230641f2835869037d312abd0f50688f6
jorge luis camarillo <jlcc170@gmail.com>
2020-01-29 03:17:42 -0600

    ADD gitignore config

--- removeDir.js
+++ removeDir.js
@@ -8,21 +8,30 @@ function removeDirRecursive(filePath) {
 
     // if file : unlinkSync 
     if (stats.isFile()) {
-      fs.unlinkSync(filePath);
-      console.log(colors.success(`Archivo ${filePath} eliminado`))
+      fs.unlink(filePath, (err) => {
+        if (err){
+          console.log(colors.error(`El archivo ${filePath} no se puede eliminar`))
+          return
+        }
+        console.log(colors.success(`Archivo ${filePath} eliminado`))
+      });
     }
 
     //if is a directory, readdir() and call removeDirRecursive() for each file
     else {
       let files = fs.readdirSync(filePath);
       files.forEach((file) => {
-        console.log(filePath + '/' + file)
         removeDirRecursive(filePath + '/' + file);
+        console.log(filePath + '/' + file)
       });
-      fs.rmdirSync(filePath);
-      console.log(colors.success(`El directorio ${filePath} fue eliminado`))
+      fs.rmdir(filePath, err => {
+        if (err){
+          console.log(colors.error(`El directorio ${filePath} no se pudo eliminar`))
+          return
+        }
+        console.log(colors.success(`El directorio ${filePath} fue eliminado`))
+      });
     }
-    
   } else {
     console.log(colors.error(`El directorio ${filePath} no existe`))
   }
